b00c0420b9adfca3959cd6c03a2609497cbcb3c3
Lucman Abdulrachman <lucman.abdulrachman@msumain.edu.ph>
2021-02-10 12:43:14 +0800

    establish connection

--- intro-to-riak.js
+++ intro-to-riak.js
@@ -1,1 +1,38 @@
-console.log("hello with riak");
+var RiakClient = require('basho-riak-client').Client;
+
+var riakNodes = ['localhost:8087'];
+
+async function main() {
+  var client = await createClientConnection();
+  stopClientConnection(client);
+}
+main();
+
+async function createClientConnection() {
+  return new Promise((resolve, reject) => {
+    var riakClient = new RiakClient(riakNodes, (err, client) => {
+      if (err) {
+        reject(err);
+      }
+      console.log("connection is established");
+      riakClient.ping((err) => {
+        if (err) {
+          reject(err);
+        }
+        console.log("ping is successful");
+        resolve(riakClient);
+      });
+    })
+  })
+
+}
+
+function stopClientConnection(client) {
+  client.stop((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("connection is closed");
+    }
+  })
+}
